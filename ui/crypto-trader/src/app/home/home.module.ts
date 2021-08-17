8d7ac640373a3645885e87c4b0fe0e360125b539
Dominik2122 <dominik.iwanek@gmail.com>
2021-08-17 21:27:49 +0200

    17082021-2

--- ui/crypto-trader/src/app/home/home.module.ts
+++ ui/crypto-trader/src/app/home/home.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRootComponent } from './home-root/home-root.component';
 import {TreeModule} from "src/app/util/tree/tree.module";
+import {HomeRoutingModule} from "src/app/home/home-routing.module";
 
 
 
 @NgModule({
   declarations: [
-    HomeRootComponent
+    HomeRootComponent,
+
   ],
   imports: [
     CommonModule,
+    HomeRoutingModule,
     TreeModule
   ]
 })
