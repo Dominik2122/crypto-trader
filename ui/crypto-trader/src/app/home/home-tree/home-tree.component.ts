40f5b69ed6ed5288792c4feefa15ad53c8dcd0f9
Dominik2122 <dominik.iwanek@gmail.com>
2021-08-25 23:06:57 +0200

    25082021-2

--- ui/crypto-trader/src/app/home/home-tree/home-tree.component.ts
+++ ui/crypto-trader/src/app/home/home-tree/home-tree.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TreeColumn} from "src/app/util/tree/ui/models/tree-column.model";
 
 @Component({
   selector: 'app-home-tree',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-tree.component.scss']
 })
 export class HomeTreeComponent implements OnInit {
+
+  homeTreeColumns: Array<TreeColumn> = TreeColumn.Builder.build(4).withHeaders([''])
 
   constructor() { }
 
--- ui/crypto-trader/src/app/util/tree/ui/models/tree-column.model.ts
+++ ui/crypto-trader/src/app/util/tree/ui/models/tree-column.model.ts
@@ -1,22 +1,65 @@
 export class TreeColumn {
 
   constructor(
-              public name: string,
-              public cssClass: string,
-              public header: string,
-              public flex: string,
-              public dragButton?: boolean,
-              public expand?: boolean,
+    public name: string,
+    public cssClass: string,
+    public header: string,
+    public flex: string,
+    public dragButton?: boolean,
+    public expand?: boolean,
   ) {
   }
 
-  buildOne( name: string,
-            cssClass: string,
-            header: string,
-            flex: string,
-            dragButton?: boolean,
-            expand?: boolean): TreeColumn {
-    return new TreeColumn(name, cssClass, header, flex, dragButton, expand);
+  static Builder = class TreeColumnBuilder implements ngOnInit {
+
+    columns: Array<TreeColumn>;
+
+    names: string
+
+    cssClasses: string
+
+    headers: string
+
+    flexes: string
+
+    dragButton?: boolean
+
+    expand?: boolean
+
+
+    static buildOne(): TreeColumn {
+      return new TreeColumn(this.name, cssClass, header, flex, dragButton, expand);
+    }
+
+    static buildBlank(): TreeColumn {
+      return new TreeColumn(null, null, null, null, null, null);
+    }
+
+    static build(amount: number): Array<TreeColumn> {
+      const columns: Array<TreeColumn> = [];
+      for (let i = 0; i < amount; i++) {
+        columns.push(TreeColumn.Builder.buildBlank())
+      }
+      return columns
+    }
+
+    withHeaders(headers: Array<string>) {
+      this.columns = this.columns.map((column: TreeColumn, index: number) => {
+        column.header = headers[index];
+        return column;
+      })
+      return this
+    }
+
+    withNames(names: Array<string>) {
+      this.columns = this.columns.map((column: TreeColumn, index: number) => {
+        column.header = names[index];
+        return column;
+      })
+      return this
+    }
+
   }
+
 
 }
