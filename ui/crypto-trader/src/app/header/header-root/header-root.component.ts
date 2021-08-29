3b827829a9c5d2834c7e6059f8faee18f3a92d80
Dominik2122 <dominik.iwanek@gmail.com>
2021-08-29 19:03:35 +0200

    29082021-2

--- ui/crypto-trader/src/app/header/header-root/header-root.component.ts
+++ ui/crypto-trader/src/app/header/header-root/header-root.component.ts
@@ -12,8 +12,6 @@ export class HeaderRootComponent implements OnInit {
   ngOnInit(): void {
     const btn = document.querySelector(".mobile-menu-button");
     const sidebar = document.querySelector(".sidebar");
-
-// add our event listener for the click
     btn.addEventListener("click", () => {
       sidebar.classList.toggle("-translate-x-full");
     });
--- ui/crypto-trader/src/app/home/home-title/home-title.component.ts
+++ ui/crypto-trader/src/app/home/home-title/home-title.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home-title',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeTitleComponent implements OnInit {
 
-  constructor() { }
+  action: string = ''
+  actions: Array<string> = ['', '', 'Invest in']
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.initTitleAction()
+  }
+
+  initTitleAction(): void {
+    let i: number = 0
+    this.action = this.actions[i];
+    setInterval(() => {
+      i = i == 2 ? 0 : ++i;
+      this.action = this.actions[i];
+    }, 5000)
   }
 
 }
