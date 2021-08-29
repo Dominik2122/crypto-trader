997d0b81bc0b1554c184b72b8a558c000b644d03
Dominik Iwanek <dominik.iwanek@gmail.com>
2021-08-29 10:16:28 +0200

    29082021-1

--- ui/crypto-trader/src/app/home/home.module.ts
+++ ui/crypto-trader/src/app/home/home.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { HomeRootComponent } from './home-root/home-root.component';
 import {HomeRoutingModule} from "src/app/home/home-routing.module";
 import {HomeTreeModule} from "src/app/home/home-tree/home-tree.module";
+import { HomeTitleComponent } from './home-title/home-title.component';
+import { HomeSkeletonComponent } from './home-root/home-skeleton/home-skeleton.component';
 
 
 
 @NgModule({
   declarations: [
     HomeRootComponent,
+    HomeTitleComponent,
+    HomeSkeletonComponent,
 
   ],
 	imports: [
